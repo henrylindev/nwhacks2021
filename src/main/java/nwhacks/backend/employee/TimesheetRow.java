aba5a8b6b182ee38a32570b599644bf98cae4de6
Henry Lin <henrylindev@gmail.com>
2021-01-09 14:14:24 -0800

    added TimesheetRow class

---
+++ src/main/java/nwhacks/backend/employee/TimesheetRow.java
@@ -0,0 +1,44 @@
+package nwhacks.backend.employee;
+
+public class TimesheetRow {
+    
+    private String sheetID;
+    private Double hours;
+    private String projectID;
+    
+    public TimesheetRow() {
+        super();
+    }
+
+    public TimesheetRow(String sheetID, Double hours, String projectID) {
+        super();
+        this.sheetID = sheetID;
+        this.hours = hours;
+        this.projectID = projectID;
+    }
+
+    public String getSheetID() {
+        return sheetID;
+    }
+
+    public void setSheetID(String sheetID) {
+        this.sheetID = sheetID;
+    }
+
+    public Double getHours() {
+        return hours;
+    }
+
+    public void setHours(Double hours) {
+        this.hours = hours;
+    }
+
+    public String getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(String projectID) {
+        this.projectID = projectID;
+    }
+    
+}
