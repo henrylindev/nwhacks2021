6afe646baea05574619945052077b460f9fe81da
Gurjot Sandher <g_sandher@hotmail.com>
2021-01-09 19:12:05 -0800

    Added incomplete List classes and refactored EditableCost

---
+++ src/main/java/nwhacks/backend/cost/CostList.java
@@ -0,0 +1,59 @@
+package nwhacks.backend.cost;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import nwhacks.backend.employee.EditableEmployee;
+import nwhacks.backend.employee.Employee;
+
+//import nwhacks.backend.cost.EditableEmployee;
+//import nwhacks.backend.cost.Employee;
+//import nwhacks.backend.cost.EmployeeManager;
+
+public class CostList implements Serializable {
+
+    @Inject @Dependent private MaterialManager materialManager;
+    @Inject @Dependent private WageManager wageManager;
+    
+    private List<EditableCost> list;
+        
+    @Inject Conversation conversation;
+    
+    public List<EditableCost> getList() {
+        if(!conversation.isTransient()) {
+            conversation.end();
+        }
+        conversation.begin();
+        if (list == null) {
+            refreshList();
+        }
+        return list;
+    }
+
+    public List<EditableEmployee> refreshList() {
+        
+        MaterialCost[] materialCost = MaterialCostManager.getAll();
+        
+        WageCost wageCost[] = WageCostManager.getAll();
+        
+        MaterialList = new ArrayList<EditableCost>();
+        WageList = new ArrayList<EditableCost>();
+        
+        for (int i = 0; i < materialCost.length; i++) {
+            MaterialList.add(new EditableCost(materialCost[i]));
+        }
+
+        for (int i = materialCost.length; i < (materialCost.length + wageCost.length); i++) {
+            WageList.add(new EditableCost(wageCost[i]));
+        }
+        
+        System.out.println("running");
+        return list;
+    }
+
+}
--- src/main/java/nwhacks/backend/cost/editableCost.java
+++ src/main/java/nwhacks/backend/cost/editableCost.java
@@ -4,17 +4,29 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
 
-public class editableCost extends Cost {
+public class EditableCost extends Cost {
     
     private boolean editable = false;
     private boolean deletable = false;
 
-    public editableCost() {
+    public EditableCost() {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+//    Material Cost
+    
+    public Editablecost() {
+        super();
+    }
+    
+//    Wage Cost
+    
+    public Editablecost() {
+        super();
+    }
 
-    public editableCost(UUID costId, UUID projectId, Date date, BigDecimal amount, boolean inJournal) {
+    public EditableCost(UUID costId, UUID projectId, Date date, BigDecimal amount, boolean inJournal) {
         super(costId, projectId, date, amount, inJournal);
         // TODO Auto-generated constructor stub
     }
